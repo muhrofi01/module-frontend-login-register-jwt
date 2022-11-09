b7753e0d5c91bc2fc2d13135c26829b71e61c349
muhrofi01 <muhrofi01@gmail.com>
2022-11-09 13:25:28 +0700

    hubungkan register dengan api

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,35 +1,61 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
 const Register = () => {
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [confPassword, setConfPassword] = useState("");
+    const [message, setMessage] = useState("");
+    const navigate = useNavigate();
+
+    const Register = async(e) => {
+        e.preventDefault();
+        try {
+            await axios.post('http://localhost:5000/users', {
+                name: name,
+                email: email,
+                password: password,
+                confPassword: confPassword
+            });
+            navigate("/");
+        } catch (error) {
+            if(error.response) {
+                setMessage(error.response.data.message);
+            }
+        }
+    }
     return (    
         <section className="hero has-background-grey-light is-fullheight is-fullwidth">
           <div className="hero-body">
             <div className="container">
                 <div className="columns is-centered">
                     <div className="column is-4-desktop">
-                        <form action="" className="box">
+                        <form action="" onSubmit={Register} className="box">
+                            <p className="has-text-centered">{ message }</p>
                             <div className="field">
                                 <label htmlFor="" className="label">Name</label>
                                 <div className="controls">
-                                    <input type="text" className="input" placeholder="Name" />
+                                    <input type="text" className="input" placeholder="Name" value={name} onChange={(e) => setName(e.target.value)} />
                                 </div>
                             </div>
                             <div className="field">
                                 <label htmlFor="" className="label">Email</label>
                                 <div className="controls">
-                                    <input type="text" className="input" placeholder="Email" />
+                                    <input type="text" className="input" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} />
                                 </div>
                             </div>
                             <div className="field">
                                 <label htmlFor="" className="label">Password</label>
                                 <div className="controls">
-                                    <input type="password" className="input" placeholder="*****" />
+                                    <input type="password" className="input" placeholder="*****" value={password} onChange={(e) => setPassword(e.target.value)}/>
                                 </div>
                             </div>
                             <div className="field">
                                 <label htmlFor="" className="label">Confirm Password</label>
                                 <div className="controls">
-                                    <input type="password" className="input" placeholder="*****" />
+                                    <input type="password" className="input" placeholder="*****" value={confPassword} onChange={(e) => setConfPassword(e.target.value)} />
                                 </div>
                             </div>
                             <div className="field">
