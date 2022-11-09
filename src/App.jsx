7071378f57dff484c56f8243835e78b22244c888
muhrofi01 <muhrofi01@gmail.com>
2022-11-09 12:58:01 +0700

    buat halaman dashboard dan navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Login from "./components/Login";
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Register from "./components/Register";
+import Dashboard from "./components/Dashboard";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route exact path="/" element={<Login />}/> 
         <Route path="/register" element={<Register />}/> 
+        <Route path="/dashboard" element={<Dashboard/>} />
       </Routes>
     </BrowserRouter>
   );
